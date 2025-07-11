6b38368394313a859175eb8ac548c984b83056fd
orimkhoirul <mkhoirulutfansyah@gmail.com>
2025-07-11 11:20:03 +0700

    update api_server.py

--- api_server.py
+++ api_server.py
@@ -45,4 +45,4 @@ def check_task_status(task_id):
     return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1',port=5001)
+    app.run(debug=True, host='0.0.0.0',port=5001)
