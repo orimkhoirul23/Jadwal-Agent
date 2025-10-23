28f56e05fa9dca67b4274e6d7982d66bc6c1bd7a
orimkhoirul <mkhoirulutfansyah@gmail.com>
2025-10-23 14:20:05 +0700

    redirect http to https for check_url api

--- api_server.py
+++ api_server.py
@@ -6,10 +6,11 @@
 from flask_cors import CORS
 from flask import Flask, request, jsonify, url_for
 from celery_task import run_solver_task
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
 CORS(app)
-
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1)
 @app.route('/generate-schedule', methods=['POST'])
 def start_schedule_generation():
     """Endpoint untuk memulai proses pembuatan jadwal."""
